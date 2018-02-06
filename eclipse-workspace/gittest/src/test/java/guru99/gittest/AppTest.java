20262ef9843f0c06e0d0648fe306eca8c5541a17
ADMIN <ADMIN@192.168.1.11>
2018-02-06 16:29:14 +0530

    Git Test Demo..

---
+++ eclipse-workspace/gittest/src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,14 @@
+package guru99.gittest;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+    
+{
+   @Test
+   public void testCase1()
+   {
+	   System.out.println("welcome togit world");
+   }
+   
+}
